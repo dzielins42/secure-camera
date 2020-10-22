b8769d6e2a3e732b901c6e4aa5e42f3ddcbc005b
dzielins42 <dzielins42@gmail.com>
2020-10-22 18:25:58 +0200

    Integrate Timber

---
+++ app/src/main/java/pl/dzielins42/seccam/MyApplication.kt
@@ -0,0 +1,16 @@
+package pl.dzielins42.seccam
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
