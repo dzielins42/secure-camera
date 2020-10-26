728630b7f343f36ed8540bbf2b89347546caacd8
dzielins42 <dzielins42@gmail.com>
2020-10-26 19:37:03 +0100

    Set default placeholder for loading images

--- app/src/main/java/pl/dzielins42/seccam/glide/MyAppGlideModule.kt
+++ app/src/main/java/pl/dzielins42/seccam/glide/MyAppGlideModule.kt
@@ -1,10 +1,15 @@
 package pl.dzielins42.seccam.glide
 
 import android.content.Context
+import androidx.core.content.ContextCompat
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+import pl.dzielins42.seccam.R
 import pl.dzielins42.seccam.data.model.FileGalleryItem
 import pl.dzielins42.seccam.data.model.UrlGalleryItem
 import java.io.InputStream
@@ -22,6 +27,23 @@ class MyAppGlideModule : AppGlideModule() {
             FileGalleryItem::class.java,
             InputStream::class.java,
             FileGalleryItemModelLoader.Factory()
+        )
+    }
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.setColorSchemeColors(
+            ContextCompat.getColor(context, R.color.colorAccent)
+        )
+        circularProgressDrawable.start()
+
+        builder.setDefaultRequestOptions(
+            RequestOptions()
+                .placeholder(circularProgressDrawable)
         )
     }
 }
