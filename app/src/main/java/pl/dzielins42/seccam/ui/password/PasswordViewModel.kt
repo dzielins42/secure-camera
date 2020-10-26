93ccaf2984f3fec15263b671778344553758488b
dzielins42 <dzielins42@gmail.com>
2020-10-26 18:57:52 +0100

    Handle exceptions from PasswordRepository in PasswordViewModel

--- app/src/main/java/pl/dzielins42/seccam/ui/password/PasswordViewModel.kt
+++ app/src/main/java/pl/dzielins42/seccam/ui/password/PasswordViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import pl.dzielins42.seccam.data.repo.PasswordRepository
+import timber.log.Timber
 
 class PasswordViewModel(
     private val passwordRepository: PasswordRepository
@@ -13,14 +14,20 @@ class PasswordViewModel(
 
     val viewState: LiveData<PasswordViewState>
         get() = mutableViewState
-    private val mutableViewState = MutableLiveData<PasswordViewState>(PasswordViewState.Idle)
+    private val mutableViewState = MutableLiveData(PasswordViewState.Idle)
 
     fun validatePassword(password: String) {
         viewModelScope.launch {
             mutableViewState.postValue(PasswordViewState.Loading)
-            val result = passwordRepository.validatePassword(password)
             mutableViewState.postValue(
-                if (result) PasswordViewState.PasswordCorrect else PasswordViewState.PasswordIncorrect
+                try {
+                    val result = passwordRepository.validatePassword(password)
+                    if (result) PasswordViewState.PasswordCorrect else PasswordViewState.PasswordIncorrect
+                } catch (e: Exception) {
+                    // Nothing can be done about the exception, but it should be handled graciously
+                    Timber.e(e)
+                    PasswordViewState.PasswordIncorrect
+                }
             )
         }
     }
